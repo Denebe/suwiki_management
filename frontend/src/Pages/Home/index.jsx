e04899608bdbbbe9bb9efbcd8c4e15f7db9d49f3
denebe <twhjdfo01@daum.net>
2022-05-10 12:36:38 +0900

    [Add]메인 페이지

--- frontend/src/Pages/Home/index.jsx
+++ frontend/src/Pages/Home/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import * as Styled from "./style";
-
+import { useNavigate } from 'react-router-dom';
 /*
 최근 신고된 글(신고된 날짜, 강의이름, 교수이름, 작성한 글, 신고 당한 횟수, 담당자)
 클릭 시 화면 이동
@@ -12,15 +12,30 @@ import * as Styled from "./style";
 */
 
 const Home = () => {
+    let navigate = useNavigate()
+
+    const db = [{id: '1',date : '2022', pro: '교수', book : '강좌', content: '내용', count: '신고당한 회수'},
+    {id: '2' ,date : '2022', pro: '교수', book : '강좌', content: '내용', count: '3'},]
   return (
     <Styled.Container>
       <Styled.TextWrapper>
+        <Styled.Button onClick={() => navigate('notice')}>공지사항으로 가기</Styled.Button>
         <Styled.Text>최근 신고된 글</Styled.Text>
       </Styled.TextWrapper>
 
       <Styled.Wrapper>
         <Styled.FullWrapSub>
-          <Subject />
+            {
+                db.map((date) => (
+                    <Subject
+                    key={date.id}
+                    date={date.date}
+                    pro={date.pro}
+                    book={date.book}
+                    content={date.content}
+                    count = {date.count} />
+                ))
+            }
         </Styled.FullWrapSub>
       </Styled.Wrapper>
     </Styled.Container>
@@ -28,20 +43,19 @@ const Home = () => {
 };
 
 export const Subject = (props) => {
-
   return (
     <Styled.LectureWrapper>
       <Styled.MarginTop>
         <Styled.TitleWrapper>
-          <Styled.Title>강좌이름</Styled.Title>
-          <Styled.Option>신고당한 회수</Styled.Option>
+          <Styled.Title>{props.book}</Styled.Title>
+          <Styled.Option>{props.count}</Styled.Option>
         </Styled.TitleWrapper>
         <Styled.TitleWrapper>
-          <Styled.Professor>교수</Styled.Professor>
-          <Styled.Option>신고된 날짜</Styled.Option>
+          <Styled.Professor>{props.pro}</Styled.Professor>
+          <Styled.Option>{props.date}</Styled.Option>
         </Styled.TitleWrapper>
         <Styled.RateWrapper>
-          <Styled.Rate>작성한 글</Styled.Rate>
+          <Styled.Rate>{props.content}</Styled.Rate>
         </Styled.RateWrapper>
       </Styled.MarginTop>
     </Styled.LectureWrapper>
