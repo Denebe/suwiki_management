d749d957524a29e3f05c55fc85d84b6d3e1cd484
denebe <twhjdfo01@daum.net>
2022-06-07 00:55:10 +0900

    [Fix]Warnings

--- frontend/src/Pages/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Login } from './Login'
-export { default as Home } from './Home'
-export { default as Write } from './Write'
-export { default as NoticeDetail } from './NoticeDetail'
-export { default as Update } from './Update'
-export { default as Report } from './Report'
