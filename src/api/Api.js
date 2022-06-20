1028bd938171b8c1125f08caeb9a3e4886aa1053
denebe <twhjdfo01@daum.net>
2022-06-20 18:08:52 +0900

    proxy

--- src/api/Api.js
+++ src/api/Api.js
@@ -11,7 +11,7 @@ export const getCookie = (name) => {
   return cookies.get(name);
 };
 
-const PROXY_URL = window.location.hostname === 'localhost' ? '' : '/proxy';
+const PROXY_URL = window.location.hostname === 'localhost' ? '' : '/';
 
 //로그인api 0
 export const loginApi = (setData, setLoading, id, pw) => {
