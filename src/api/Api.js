c1bba4bb3b88a5fe1d60a02e0aa5ecefb25a0724
denebe <twhjdfo01@daum.net>
2022-06-25 01:01:56 +0900

    proxy

--- src/api/Api.js
+++ src/api/Api.js
@@ -15,7 +15,7 @@ const PROXY_URL = window.location.hostname === 'localhost' ? '' : '/proxy';
 
 //로그인api 0
 export const loginApi = (setData, setLoading, id, pw) => {
-  const url = `${PROXY_URL}/user/login`;
+  const url = `api.suwiki.kr/user/login`;
   const data = {
     loginId: id,
     password: pw,
@@ -244,7 +244,7 @@ export const evaluateBanApi = (setData,  id, reason, judge, time) => {
   };
 
   export const noBanExamApi = (setData,  id) => {
-    const url = `${PROXY_URL}/admin/ban/exam-post`;
+    const url = `admin/ban/exam-post`;
   
     const data = {
         examIdx : id,
