aea6b0c118274e6a375fb6e72513f387d7c3360b
denebe <twhjdfo01@daum.net>
2022-07-03 19:44:33 +0900

    [Add]httponly

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { CokkieProvider } from 'react-cookie'
+import { CookiesProvider } from 'react-cookie'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CokkieProvider>
+    <CookiesProvider>
       <App />
-    </CokkieProvider>
+    </CookiesProvider>
   </React.StrictMode>
 );
 
