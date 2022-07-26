3398f0a69ca9d5c7edbda2908174a93120aacdef
denebe <twhjdfo01@daum.net>
2022-07-26 23:50:18 +0900

    fix

--- src/api/Api.js
+++ src/api/Api.js
@@ -40,7 +40,6 @@ export const loginApi = (setData, setLoading, id, pw) => {
       setLoading(true);
       setCookie("AccessToken", r.data["AccessToken"], {
         path: '/',
-        httpOnly: true,
         secure: true,
         sameSite: false,
         credentials: true
